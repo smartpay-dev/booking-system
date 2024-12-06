a04bd1b2ebf146fcd18dd907cdcd3cea8dbe071e
Rofiq Rifiansyah <piqsworkmail@gmail.com>
2024-12-06 16:37:04 +0700

    bug fixing login

--- application/models/M_user.php
+++ application/models/M_user.php
@@ -1,11 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class M_User extends CI_Model {
+class M_user extends CI_Model {
 
-    public function __construct() {
-        parent::__construct();
+    public function getUserByUsername($username) {
+        $query = $this->db->get_where('tb_user', ['username' => $username]);
+        return $query->row_array();
     }
-
-
 }
